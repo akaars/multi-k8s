b688b16d9b499d974eaca28f9bbbdd77258ec78c
Ilya Brik <ilya@teridion.com>
2021-04-01 08:21:40 +0300

    Fixed unclosed tags

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fib calculator version K8S</h1>
-          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
